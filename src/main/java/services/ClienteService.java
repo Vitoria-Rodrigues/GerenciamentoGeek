04a38f21426db5afbaca040310bdbf910d79bcc0
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-10-28 17:51:47 -0300

    Feat: adicionar refatoração inicial de ClienteDAO usando a classe ClienteService

---
+++ src/main/java/services/ClienteService.java
@@ -0,0 +1,82 @@
+
+package services;
+
+import classes.Cliente;
+import classesDAO.ClienteDAO;
+import java.util.List;
+import validacao.Alerta;
+
+
+public class ClienteService {
+    private ClienteDAO clienteDAO = new ClienteDAO();
+    
+    public void cadastrarCliente(Cliente cliente){
+        try{
+            clienteDAO.salvar(cliente);
+            Alerta.Sucesso("Cadastro concluído!", "Cliente cadastrado com sucesso!");
+        } catch(Exception e){
+            Alerta.Erro("Erro no cadastro", "Erro ao inserir o cadastro no banco");
+        }
+    }
+    
+    public void editarCliente(Cliente cliente){
+        try{
+            clienteDAO.atualizar(cliente);
+            Alerta.Sucesso("Sucesso!", "Edição realizar com sucesso!");
+        } catch(Exception e){
+            Alerta.Erro("Erro ao editar", "Ocorreu um erro ao editar as informações");
+        }
+    }
+    
+    public List<Cliente> listarClientes(String cpf){
+        try{
+            if(cpf == null || cpf.isEmpty()){
+                return clienteDAO.listarTodos();
+            }
+            return clienteDAO.buscarPorCPF(cpf);
+        }catch (Exception e){
+            Alerta.Erro("Erro listagem", "Erro ao buscar informação para lista");
+            return List.of();
+        }
+    }
+    
+    public Cliente buscarCliente(String idCliente){
+        try{
+            return clienteDAO.buscarPorId(idCliente);
+        }catch(Exception e){
+            Alerta.Erro("Erro", "Erro ao listar o cliente");
+            return null;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
