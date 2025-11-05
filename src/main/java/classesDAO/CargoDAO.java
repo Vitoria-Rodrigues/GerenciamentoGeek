22bb3e8c7fa72709dfc452dc8aaaaaf4ec0eb51d
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-11-05 14:31:48 -0300

    Feat: adicionar cargo com metodos de busca por id e listagem de cargos

---
+++ src/main/java/classesDAO/CargoDAO.java
@@ -0,0 +1,31 @@
+
+package classesDAO;
+
+import classes.Cargo;
+import classes.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+public class CargoDAO {
+    
+    public Cargo buscarPorId(Long id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.find(Cargo.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Cargo> pegarCargos(){
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            TypedQuery<Cargo> query = em.createQuery("SELECT c FROM Cargos c", Cargo.class);
+            return query.getResultList();
+        } finally{
+            em.close();
+        }
+    }
+
+}
