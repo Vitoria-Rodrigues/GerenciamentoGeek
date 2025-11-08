f7f34debb7e87f6ead8ab1d24ea1baf1f77304af
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-11-08 16:31:04 -0300

    Refactor: retaforar metodo de cadastro de venda

--- src/main/java/classesDAO/FormaPagamentoDAO.java
+++ src/main/java/classesDAO/FormaPagamentoDAO.java
@@ -4,24 +4,21 @@ package classesDAO;
 import classes.JPAUtil;
 import jakarta.persistence.EntityManager;
 import classes.FormaPagamento;
-import validacao.Alerta;
 
 public class FormaPagamentoDAO {
-    public static void cadastrarVenda(FormaPagamento formapagamento) {
+    public void cadastrarVenda(FormaPagamento formaPagamento) {
         EntityManager em = JPAUtil.getEntityManager();
+
         try {
             em.getTransaction().begin();
-            em.persist(formapagamento.getVenda());
-            em.persist(formapagamento);
+            em.persist(formaPagamento.getVenda()); 
+            em.persist(formaPagamento);             
             em.getTransaction().commit();
-            Alerta.Sucesso("Cadastro concluído!", "Venda cadastrado com sucesso!");
-
         } catch (Exception e) {
-            e.printStackTrace();
-            em.getTransaction().rollback();    
-            Alerta.Erro("Erro no cadastro", "Erro ao inserir o cadastro no banco");
+            em.getTransaction().rollback();
+            throw e; 
         } finally {
-            JPAUtil.closeEntityManager();
+            em.close();
         }
     }
     
