5445e9a7787279aabc42201c95a54740fee90f04
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-11-19 15:13:35 -0300

    Refactor: substituir forma de cadastrar e editar funcionarios usando DTO em vez da classe Funcionario

--- src/main/java/services/FuncionarioService.java
+++ src/main/java/services/FuncionarioService.java
@@ -1,80 +1,52 @@
 
 package services;
 
+import DTO.FuncionarioDTO;
 import classes.Cargo;
 import classes.Funcionario;
+import classes.Login;
 import classesDAO.CargoDAO;
 import classesDAO.FuncionarioDAO;
 import java.util.List;
 
 public class FuncionarioService {
     private final FuncionarioDAO funcionarioDAO;
-    private final CargoService cargoService;
+    private final CargoDAO cargoDAO;
 
     public FuncionarioService() {
         this.funcionarioDAO = new FuncionarioDAO();
-        this.cargoService = new CargoService();
+        this.cargoDAO = new CargoDAO();
     }
 
-    public void salvarFuncionario(Funcionario funcionario) {
-        validacaoFuncionario(funcionario);
+    public void salvarFuncionario(FuncionarioDTO funcionarioDTO) {
+        Login login = new Login(funcionarioDTO.getLogin(), funcionarioDTO.getSenha());
+        Cargo cargo = cargoDAO.buscarPorId(funcionarioDTO.getCargo());
+        
+        Funcionario funcionario = new Funcionario(funcionarioDTO.getNome(), funcionarioDTO.getCpf(), funcionarioDTO.getLogradouro(), 
+        funcionarioDTO.getCep(), funcionarioDTO.getNumero(), funcionarioDTO.getComplemento(), funcionarioDTO.getTelefone(), 
+        login, cargo);
+        
         funcionarioDAO.salvar(funcionario);
     }
     
-    public void atualizarFuncionario(Funcionario funcionario) {
-        if (funcionario == null || funcionario.getId() == null) {
-            throw new IllegalArgumentException("Funcionário inválido para atualização!");
-        }
-        validacaoFuncionario(funcionario);
-        funcionarioDAO.atualizar(funcionario);
+    public void atualizarFuncionario(FuncionarioDTO funcionarioDTO, Long id) {
+        Cargo cargo = cargoDAO.buscarPorId(funcionarioDTO.getCargo());
+        
+        Funcionario funcionarioEdit = funcionarioDAO.buscarPorId(id);
+        funcionarioEdit.setNomeF(funcionarioDTO.getNome());
+        funcionarioEdit.setCpfF(funcionarioDTO.getCpf());
+        funcionarioEdit.setLogradouro(funcionarioDTO.getLogradouro());
+        funcionarioEdit.setCep(funcionarioDTO.getCep());
+        funcionarioEdit.setNumero(funcionarioDTO.getNumero());
+        funcionarioEdit.setComplemento(funcionarioDTO.getComplemento());
+        funcionarioEdit.setTelefoneF(funcionarioDTO.getTelefone());
+        funcionarioEdit.getLogin().setSenha(funcionarioDTO.getSenha());
+        funcionarioEdit.setCargo(cargo);
+        
+        funcionarioDAO.atualizar(funcionarioEdit);
     }
     
-    private void validacaoFuncionario(Funcionario funcionario){
-        if (funcionario == null) {
-            throw new IllegalArgumentException("Funcionário não pode ser nulo!");
-        }
-
-        else if (funcionario.getNomeF() == null || funcionario.getNomeF().isBlank()) {
-            throw new IllegalArgumentException("O nome do funcionário é obrigatório!");
-        }
-
-        else if (funcionario.getCpfF() == null || funcionario.getCpfF().isBlank()) {
-            throw new IllegalArgumentException("O CPF do funcionário é obrigatório!");
-        }
-        
-        else if (funcionario.getCep() == null || funcionario.getCep().isBlank()) {
-            throw new IllegalArgumentException("O CEP do funcionário é obrigatório!");
-        }
-        
-        else if (funcionario.getLogradouro() == null || funcionario.getLogradouro().isBlank()) {
-            throw new IllegalArgumentException("O Logradouro do funcionário é obrigatório!");
-        }
-        
-        else if (funcionario.getNumero() == null || funcionario.getNumero().isBlank()) {
-            throw new IllegalArgumentException("O Numero da residencia do funcionário é obrigatório!");
-        }
-        
-        else if (funcionario.getComplemento() == null || funcionario.getComplemento().isBlank()) {
-            throw new IllegalArgumentException("O complemento do funcionário é obrigatório!");
-        }
-        
-        else if (funcionario.getTelefoneF()== null || funcionario.getTelefoneF().isBlank()) {
-            throw new IllegalArgumentException("O complemento do funcionário é obrigatório!");
-        }     
-        
-        else if (funcionario.getCargo() == null || funcionario.getCargo().getId() == null) {
-            throw new IllegalArgumentException("O funcionário deve possuir um cargo válido!");
-        }
-        
-        List<Cargo> cargos = cargoService.listarCargos();
-
-        Cargo cargoExistente = cargoService.buscarPorId(funcionario.getCargo().getId());
-        if (cargoExistente == null) {
-            throw new IllegalArgumentException("O cargo informado não existe no banco de dados!");
-        }
-    }
-
-    public Funcionario buscarPorId(String id) {
+    public Funcionario buscarPorId(Long id) {
         return funcionarioDAO.buscarPorId(id);
     }
 
@@ -83,17 +55,10 @@ public class FuncionarioService {
     }
 
     public List<Funcionario> buscarPorCPF(String cpf) {
-        if (cpf == null || cpf.isBlank()) {
-            throw new IllegalArgumentException("CPF não pode estar vazio!");
-        }
         return funcionarioDAO.buscarPorCPF(cpf);
     }
 
     public void excluirFuncionario(String id) {
-        if (id == null || id.isBlank()) {
-            throw new IllegalArgumentException("ID do funcionário inválido!");
-        }
-
         funcionarioDAO.excluir(id);
     }
 }
