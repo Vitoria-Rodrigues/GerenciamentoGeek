29c7bca20b06df10318d051b1168814912f7741b
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-11-21 21:22:12 -0300

    Refactor: substituir informações da classe Produto para a classe ProdutoDTO em cadastrar e editar

--- src/main/java/services/ProdutoService.java
+++ src/main/java/services/ProdutoService.java
@@ -1,42 +1,49 @@
 
 package services;
 
+import DTO.ProdutoDTO;
 import classes.Categoria;
 import classes.Produto;
+import classesDAO.CategoriaDAO;
 import classesDAO.ProdutoDAO;
 import java.util.List;
 
 public class ProdutoService {
     private final ProdutoDAO produtoDAO;
+    private final CategoriaDAO categoriaDAO;
 
     public ProdutoService() {
         this.produtoDAO = new ProdutoDAO();
+        this.categoriaDAO = new CategoriaDAO();
     }
 
-    public void cadastrarProduto(Produto produto) {
-        validarCamposObrigatorios(produto);
+    public void cadastrarProduto(ProdutoDTO produtoDTO) {
+        Categoria categoria = categoriaDAO.buscarPorId(produtoDTO.getCategoria());
+        
+        Produto produto = new Produto(produtoDTO.getNome(), produtoDTO.getDesc(), produtoDTO.getPreco(),
+        produtoDTO.getQtdEstoque(), produtoDTO.getCodigo(), categoria);
         produtoDAO.cadastrarProduto(produto);
     }
 
-    public void editarProduto(Produto produto) {
-        if (produto == null || produto.getId()== null) {
-            throw new IllegalArgumentException("Produto inválido para edição.");
-        }
-        validarCamposObrigatorios(produto);
-        produtoDAO.editarProduto(produto);
+    public void editarProduto(ProdutoDTO produtoDTO, Long id) {
+        Categoria categoria = categoriaDAO.buscarPorId(produtoDTO.getCategoria());
+        
+        Produto produtoEdit = produtoDAO.buscarPorId(id);
+        produtoEdit.setNomeProd(produtoDTO.getNome());
+        produtoEdit.setDescProd(produtoDTO.getDesc());
+        produtoEdit.setPreco(produtoDTO.getPreco());
+        produtoEdit.setQtdEstoque(produtoDTO.getQtdEstoque());
+        produtoEdit.setCodigoProd(produtoDTO.getCodigo());
+        produtoEdit.setCategoria(categoria);
+        
+        produtoDAO.editarProduto(produtoEdit);
     }
 
-    public void excluirProdutos(String id) {
-        if (id == null || id.isBlank()) {
-            throw new IllegalArgumentException("O ID do produto é obrigatório para exclusão.");
-        }
+    public void excluirProdutos(String id) { 
         produtoDAO.excluirProdutos(id);
     }
     
     public Produto buscarPorId(Long idProduto) {
-        if (idProduto == null || idProduto <= 0) {
-            throw new IllegalArgumentException("O ID do produto é obrigatório para busca.");
-        }
         return produtoDAO.buscarPorId(idProduto);
     }
 
@@ -46,35 +53,5 @@ public class ProdutoService {
 
     public List<Categoria> pegarCategoria() {
         return produtoDAO.pegarCategoria();
-    }
-
-    private void validarCamposObrigatorios(Produto produto) {
-        if (produto == null) {
-            throw new IllegalArgumentException("O produto não pode ser nulo.");
-        }
-
-        if (produto.getNomeProd() == null || produto.getNomeProd().isBlank()) {
-            throw new IllegalArgumentException("O nome do produto é obrigatório.");
-        }
-
-        if (produto.getPreco() == null || produto.getPreco() <= 0) {
-            throw new IllegalArgumentException("O preço do produto deve ser maior que zero.");
-        }
-
-        if (produto.getCodigoProd() <= 0) {
-            throw new IllegalArgumentException("O código do produto é obrigatório.");
-        }
-
-        if (produto.getQtdEstoque() <= 0) {
-            throw new IllegalArgumentException("A quantidade do produto deve ser zero ou maior.");
-        }
-
-        if (produto.getDescProd() == null || produto.getDescProd().isBlank()) {
-            throw new IllegalArgumentException("A descrição do produto é obrigatória.");
-        }
-
-        if (produto.getCategoria() == null || produto.getCategoria().getId() == null) {
-            throw new IllegalArgumentException("A categoria do produto é obrigatória.");
-        }
     }
 }
