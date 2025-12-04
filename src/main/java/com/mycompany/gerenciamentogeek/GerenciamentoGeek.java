0deab4bce5f72f32d70389aee6274e2f0a4cd2fb
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-12-04 18:09:39 -0300

    Feat: adicionar testes dos metodos da cadastrar, exibir e excluir na main

--- src/main/java/com/mycompany/gerenciamentogeek/GerenciamentoGeek.java
+++ src/main/java/com/mycompany/gerenciamentogeek/GerenciamentoGeek.java
@@ -1,13 +1,23 @@
 
 package com.mycompany.gerenciamentogeek;
-import telas.TelaInicial;
+
+import DTO.ClienteDTO;
+import classes.Cliente;
+import controllers.ClienteController;
+import java.util.List;
 
 public class GerenciamentoGeek {
 
     public static void main(String[] args) {
-        TelaInicial tela = new TelaInicial();
-        tela.setVisible(true);
-        tela.setLocationRelativeTo(null);
+        ClienteController cc = new ClienteController();
+        ClienteDTO clienteDTO = new ClienteDTO("Tupynamba Lucas", "14252125199",
+                "Feminino", "11955443322");
+        cc.cadastrarCliente(clienteDTO);
         
+        Cliente cliente = cc.buscarClientePorCPF("14252125199");
+        
+        List<Cliente> listarCliente = cc.listarClientes();
+        
+        cc.excluirCliente("2");
     }
 }
